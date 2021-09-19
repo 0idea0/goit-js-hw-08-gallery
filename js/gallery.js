c3fbc9f99c14d3e274ec68d8007f7bc8d63a0970
KYRYLO VOITOVSKYI <kirill.voitovskiy@gmail.com>
2021-09-19 14:28:48 +0300

    Добавляет переключение по галерее стрелками

--- js/gallery.js
+++ js/gallery.js
@@ -7,23 +7,23 @@ const buttonRef = document.querySelector('.lightbox__button');
 
 
 const markup = galleryItems.map(
-    ({ preview, original, description }, index) =>
-      `
+  ({ preview, original, description }, index) =>
+    `
     <li class="gallery__item">
         <a class="gallery__link" href=''>
             <img class="gallery__image" src="${preview}" data-source="${original}" 
             alt="${description}" data-index="${index}"/>
        </a>
     </li>`,
-  )
+)
   .join('');
 
 galleryListRef.insertAdjacentHTML('afterbegin', markup);
 
 const onOpenModalClick = e => {
-    e.preventDefault();
+  e.preventDefault();
 
-    if (e.target.localName === 'img') {
+  if (e.target.localName === 'img') {
     modalImgRef.src = e.target.dataset.source;
     modalImgRef.alt = e.target.alt;
     modalImgRef.dataset.index = e.target.dataset.index;
@@ -50,5 +50,37 @@ const onCloseModalClick = e => {
 galleryListRef.addEventListener('click', onOpenModalClick);
 window.addEventListener('keyup', onKeyboardClick);
 window.addEventListener('click', onCloseModalClick);
+
+window.addEventListener('keydown', e => {
+  if (e.code === 'ArrowLeft') {
+    onArrowLeft();
+  }
+  if (e.code === 'ArrowRight') {
+    onArrowRight();
+  }
+});
+
+function onArrowLeft() {
+  let index = +modalImgRef.dataset.index;
+  if (index === 0) {
+    newSrc(galleryItems.length - 1);
+    return;
+  }
+  newSrc(index, -1);
+}
+
+function onArrowRight() {
+  let index = +modalImgRef.dataset.index;
+  if (index === galleryItems.length - 1) {
+    newSrc(0);
+    return;
+  }
+  newSrc(index, 1);
+}
+
+function newSrc(index, step = 0) {
+  modalImgRef.dataset.index = `${index + step}`;
+  modalImgRef.src = galleryItems[index + step].original;
+}
 
 
